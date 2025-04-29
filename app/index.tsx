3f05caa3323eb144773543cf39bb171a3c674651
Erika Manuella <erika.manuella@academico.ifpb.edu.br>
2025-04-29 11:32:58 -0300

    Lista de usuarios

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,31 @@
 import {StyleSheet, Text, View } from "react-native";
 
+const usuarios = [
+  {
+    id: 1,
+    nome: "Erika",
+    cargo: "Professor"
+
+  },
+  {
+    id: 2,
+    nome: "Valentina",
+    cargo: "Aluno"
+  }
+];
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Olá</Text>
-      <Text>Olá</Text>
-      <Text>Olá</Text>
+      style={style.container}
+      >
+      {
+        usuarios.map(item => (
+          <view style={fontStyle.itemLista}>
+          <text style={style.textoLista}>{item.nome}</text>
+          </view>
+        ))
+      }
+      
     </View>
   );
 }
@@ -21,5 +35,16 @@ const style = StyleSheet.create ({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  itemLista:{
+    width: "100%",
+    padding: 10,
+    marginBottom: 2,
+    backgroundColor: "blue"
+  },
+
+  textoLista:{
+    fontSize: 20
   }
 })
